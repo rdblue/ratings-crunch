a7c5b30676fec032fc91a9db6de5bf45b38a0991
Ryan Blue <blue@apache.org>
2015-01-28 11:19:14 -0800

    Add output to a Hive dataset of movie histograms.

--- src/main/java/org/kitesdk/examples/movies/AnalyzeRatings.java
+++ src/main/java/org/kitesdk/examples/movies/AnalyzeRatings.java
@@ -15,24 +15,30 @@
  */
 package org.kitesdk.examples.movies;
 
+import com.google.common.collect.Lists;
 import java.io.Serializable;
+import java.util.Collection;
 import org.apache.crunch.FilterFn;
 import org.apache.crunch.MapFn;
 import org.apache.crunch.PCollection;
-import org.apache.crunch.PTable;
 import org.apache.crunch.Pair;
-import org.apache.crunch.TupleN;
+import org.apache.crunch.Target;
 import org.apache.crunch.util.CrunchTool;
 import org.apache.hadoop.util.ToolRunner;
 import org.kitesdk.data.Dataset;
+import org.kitesdk.data.DatasetDescriptor;
 import org.kitesdk.data.Datasets;
 import org.kitesdk.data.crunch.CrunchDatasets;
 
+import static org.apache.crunch.types.avro.Avros.collections;
 import static org.apache.crunch.types.avro.Avros.ints;
 import static org.apache.crunch.types.avro.Avros.longs;
-import static org.apache.crunch.types.avro.Avros.tuples;
+import static org.apache.crunch.types.avro.Avros.specifics;
 
 public class AnalyzeRatings extends CrunchTool implements Serializable {
+
+  public static final String IN_DATASET_URI = "dataset:hive:ratings";
+  public static final String OUT_DATASET_URI = "dataset:hive:ratings_histograms";
 
   public AnalyzeRatings(boolean inMemory) {
     super(inMemory);
@@ -40,18 +46,34 @@ public class AnalyzeRatings extends CrunchTool implements Serializable {
 
   @Override
   public int run(String[] args) throws Exception {
-    Dataset<Rating> ratings = Datasets.load("dataset:hive:ratings", Rating.class);
+    // load or create the output dataset
+    Dataset<MovieRatingsHistogram> histograms;
+    if (Datasets.exists(OUT_DATASET_URI)) {
+      histograms = Datasets.load(OUT_DATASET_URI,
+          MovieRatingsHistogram.class);
+    } else {
+      histograms = Datasets.create(OUT_DATASET_URI,
+          new DatasetDescriptor.Builder()
+              .schema(MovieRatingsHistogram.class)
+              .build(),
+          MovieRatingsHistogram.class);
+    }
 
-    PCollection<Rating> collection = read(CrunchDatasets.asSource(ratings));
-    PTable<Long, TupleN> table = collection
+    PCollection<Rating> ratings = read(CrunchDatasets.asSource(
+        Datasets.load(IN_DATASET_URI, Rating.class)));
+
+    PCollection<MovieRatingsHistogram> bimodal = ratings
         .by(new GetMovieID(), longs())
         .mapValues(new GetRating(), ints())
         .groupByKey()
-        .mapValues(new BuildRatingsHistogram(),
-            tuples(ints(), ints(), ints(), ints(), ints()))
+        .mapValues(new AccumulateRatings(), collections(ints()))
+        .parallelDo(new BuildRatingsHistogram(),
+            specifics(MovieRatingsHistogram.class))
         .filter(new IdentifyBimodalMovies());
 
-    writeTextFile(table, "bimodal_movies");
+    bimodal.write(
+        CrunchDatasets.asTarget(histograms),
+        Target.WriteMode.OVERWRITE);
 
     return getPipeline().done().succeeded() ? 0 : 1;
   }
@@ -70,45 +92,42 @@ public class AnalyzeRatings extends CrunchTool implements Serializable {
     }
   }
 
-  public static class AverageRating extends MapFn<Iterable<Integer>, Double> {
+  public static class AccumulateRatings
+      extends MapFn<Iterable<Integer>, Collection<Integer>> {
     @Override
-    public Double map(Iterable<Integer> ratings) {
-      long sum = 0;
-      int count = 0;
-      for (Integer rating : ratings) {
-        sum += rating;
-        count += 1;
-      }
-      return ((double) sum) / count;
-    }
-  }
-
-  public static class BuildRatingsHistogram extends MapFn<Iterable<Integer>, TupleN> {
-    @Override
-    public TupleN map(Iterable<Integer> ratings) {
+    public Collection<Integer> map(Iterable<Integer> ratings) {
       int[] counts = new int[] {0, 0, 0, 0, 0};
       for (int rating : ratings) {
         if (rating <= 5) {
           counts[rating - 1] += 1;
         }
       }
-      return new TupleN(counts[0], counts[1], counts[2], counts[3], counts[4]);
+      return Lists.newArrayList(
+          counts[0], counts[1], counts[2], counts[3], counts[4]);
+    }
+  }
+
+  public static class BuildRatingsHistogram
+      extends MapFn<Pair<Long, Collection<Integer>>, MovieRatingsHistogram> {
+    @Override
+    public MovieRatingsHistogram map(Pair<Long, Collection<Integer>> movieIdAndRatings) {
+      return new MovieRatingsHistogram(movieIdAndRatings.first(),
+          Lists.newArrayList(movieIdAndRatings.second()));
     }
   }
 
   // require a difference of at least 2 to register a change
   public static final int CHANGE_THRESH = 2;
 
-  public static class IdentifyBimodalMovies extends FilterFn<Pair<Long, TupleN>> {
+  public static class IdentifyBimodalMovies extends FilterFn<MovieRatingsHistogram> {
     @Override
-    public boolean accept(Pair<Long, TupleN> movieRatingHistograms) {
-      TupleN ratingsHistogram = movieRatingHistograms.second();
+    public boolean accept(MovieRatingsHistogram movieRatingHistogram) {
       int last = -1;
       int totalRatings = 0;
       boolean foundNegative = false;
       boolean foundBimodal = false;
-      for (Object countObj : ratingsHistogram.getValues()) {
-        int count = (Integer) countObj;
+
+      for (Integer count : movieRatingHistogram.getHistogram()) {
         totalRatings += count;
 
         if (last >= 0) {
